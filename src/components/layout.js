a25961efc17449319176374583807bb18be61e4d
matt-schaller <mschaller3@gmail.com>
2019-08-03 12:19:10 -0400

    Fix case sensitivity

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "gatsby"
-import Header from "../components/Header"
+import Header from "../components/header"
 import Hero from "../components/hero"
 import Footer from "../components/footer"
 
