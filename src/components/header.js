cb4b2644795fbe1aa166fb8f93b87049bccb6cb0
matt-schaller <mschaller3@gmail.com>
2019-08-03 14:05:50 -0400

    Header refactoring

--- src/components/header.js
+++ src/components/header.js
@@ -12,37 +12,40 @@ function Header({title}) {
 
   return (
     <section>
-      <nav class="navbar is-dark">
-        <div class="container">
-          <div class="navbar-brand">
-            <Link to={`/`}>
-              {title}
-            </Link>
-            <span class="navbar-burger burger" data-target="navbarMenuHeroA">
-              <span></span>
-              <span></span>
-              <span></span>
-            </span>
-          </div>
-          <div id="navbarMenuHeroA" class="navbar-menu">
-            <div class="navbar-end">
-            <Link className="navbar-item is-active" to={`/`}>
-              home
-            </Link>
+      <nav class="navbar is-transparent" role="navigation" aria-label="main navigation">
+
+        <div class="navbar-brand">
+          <Link className="navbar-item" to={`/`}>
+            {title}
+          </Link>
+          <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+
+        <div id="navbarMenuHeroA" class="navbar-menu">
+          <div class="navbar-start">
             <Link className="navbar-item" to={`/blog`}>
               blog
             </Link>
-              <span class="navbar-item">
-                <a href="google.com" class="button is-primary is-inverted">
-                  <span class="icon">
-                    <i class="fab fa-github"></i>
-                  </span>
-                  <span>Download</span>
-                </a>
-              </span>
-            </div>
+            <Link className="navbar-item " to={`/contact`}>
+              contact
+            </Link>
+          </div>
+          <div class="navbar-end">
+            <span class="navbar-item">
+              <a href="google.com" class="button is-primary is-inverted">
+                <span class="icon">
+                  <i class="fab fa-github"></i>
+                </span>
+                <span>Download</span>
+              </a>
+            </span>
           </div>
         </div>
+
       </nav>
     </section>
   )
