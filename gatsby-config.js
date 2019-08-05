424f48d602ef01f1eb59bf0ecb19525927244665
matt-schaller <mschaller3@gmail.com>
2019-08-05 11:15:45 -0400

    Remove blog pages from sitemap.

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-sitemap`,
       options: {
-        exclude: ["/blog*"]
+        exclude: ["/blog*", "/blog/*"]
       }
     },
     `gatsby-plugin-sass`,
