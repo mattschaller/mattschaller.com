36bf2d9e9a9b40a4bd5bd90bdbad398be69f9b55
matt-schaller <mschaller3@gmail.com>
2019-08-08 00:01:50 -0400

    Breaking blog down into components

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,13 +2,16 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import Bio from "../components/bio"
 import Layout from "../components/layout"
+import Comment from "../components/blog/comment"
+import Image from "gatsby-image"
 
 class BlogPostTemplate extends React.Component {
   render() {
     console.log(this);
-    const post = this.props.data.posts.getPost
+    const post = this.props.data.posts.getPost || {}
+    const comments = this.props.data.posts.getPost.comments.items || []
     const { previous, next } = this.props.pageContext
-
+    const portrait_thumbnail = "https://bulma.io/images/placeholders/128x128.png"
     return (
       <Layout
         location={this.props.location}
@@ -50,8 +53,53 @@ class BlogPostTemplate extends React.Component {
                 )}
               </nav>
             </div>
+
+            {comments.length ? (
+              comments.map(
+                (items) => {
+                  return (
+                    <Comment id={items.id} />
+                  )
+                }
+              )
+            ) : (
+              <article class="media">
+                No comments
+              </article>
+            )};
+
+            <article class="media">
+              <figure class="media-left">
+                <p class="image is-64x64">
+                  <Image fixed={this.props.data.portrait.childImageSharp.fixed} />
+                </p>
+              </figure>
+              <div class="media-content">
+                <div class="field">
+                  <p class="control">
+                    <textarea class="textarea" placeholder="Add a comment..."></textarea>
+                  </p>
+                </div>
+                <nav class="level">
+                  <div class="level-left">
+                    <div class="level-item">
+                      <a class="button is-info">Submit</a>
+                    </div>
+                  </div>
+                  <div class="level-right">
+                    <div class="level-item">
+                      <label class="checkbox">
+                        <input type="checkbox"/> Press enter to submit
+                      </label>
+                    </div>
+                  </div>
+                </nav>
+              </div>
+            </article>
+
           </section>
         </article>
+
       </Layout>
     )
   }
@@ -61,6 +109,13 @@ export default BlogPostTemplate
 
 export const pageQuery = graphql`
 query getPost($id: ID!) { 
+  portrait: file(absolutePath: { regex: "/128x128.png/" }) {
+    childImageSharp {
+      fixed(width: 64, height: 64) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
   posts {
     getPost(id: $id) {
       id
