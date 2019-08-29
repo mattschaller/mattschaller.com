7d824a2f416e18db5737b2b6b08bfc3b953d5e5b
matt-schaller <mschaller3@gmail.com>
2019-08-29 08:00:10 -0400

    Revert copy change

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,14 +4,11 @@ import ContactForm from "../components/contactform"
 
 class Contact extends React.Component {
   render() {
-    var referer = document.referrer;
-    var title = (referer.indexOf('formspree.io/thanks') > -1) ? "Thank you": "Contact me";
-    var subtitle = (referer.indexOf('formspree.io/thanks') > -1) ? "I will reply shortly": "Lets get in touch and talk about stuff.";
     return (
       <Layout
         location={this.props.location}
-        title={title}
-        subtitle={subtitle}
+        title="Contact me"
+        subtitle="Lets get in touch and talk about stuff."
         fullsize="false"
       >
         <ContactForm />
